04ba098a5646001260f55a644c61b66f9fb6d9a1
ChePlusPlus <chephoto@rocketmail.com>
2025-05-08 03:57:17 +0200

    Adds portfolio link to footer

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -3,7 +3,12 @@ import styles from "./footer.module.css";
 const Footer = () => {
   return (
     <footer tabIndex={-1} className={styles.footer}>
-      <p>&copy; {new Date().getFullYear()} AI Reading App. All rights reserved.</p>
+      <p>
+        {new Date().getFullYear()} AI Reading App. All rights reserved. Created by
+        <a href="https://cheovermeyer.com" target="_blank">
+          Che Overmeyer
+        </a>
+      </p>
       <a href="/privacy-policy">
         <p>Privacy Policy</p>
       </a>
