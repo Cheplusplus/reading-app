b51222f6e80892da9c7ff5dada4a060b5b098184
ChePlusPlus <chephoto@rocketmail.com>
2025-05-15 14:06:23 +0200

    Adds a MenuButton test suite

---
+++ src/app/components/MenuButton/MenuButton.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import MenuButton from "./MenuButton";
+
+jest.mock("@auth0/nextjs-auth0/client", () => ({
+  ...jest.requireActual("@auth0/nextjs-auth0/client"),
+  useUser: () => ({
+    user: { name: "Test User" },
+    error: null,
+    isLoading: false,
+  }),
+}));
+
+describe("MenuButton", () => {
+  it("Opens the MenuModal when clicked", () => {
+    render(<MenuButton />);
+    const menuButton = screen.getByRole("button", { name: "menu" });
+    fireEvent.click(menuButton);
+    const MenuModal = screen.getByTestId("container");
+
+    expect(MenuModal).toBeVisible();
+  });
+});
